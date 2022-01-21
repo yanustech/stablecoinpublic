82cf94d69715c5608a80652fde7fa412ba14f3df
yanustech <95982844+yanustech@users.noreply.github.com>
2022-01-21 14:56:25 +0100

    Update SwissSC.sol

--- Solidity/SwissSC.sol
+++ Solidity/SwissSC.sol
@@ -1,3 +1,5 @@
+pragma solidity ^0.8.0;
+
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/tree/release-v4.5/contracts/token/ERC20/ERC20.sol";
 
 contract SwissSC is ERC20 
@@ -7,4 +9,4 @@ contract SwissSC is ERC20
         
     } 
 
-}  
+}   
