d718e28f9a90d09741654e8babdc313be4b929b7
yanustech <95982844+yanustech@users.noreply.github.com>
2022-01-21 14:52:05 +0100

    Create SwissSC.sol

---
+++ Solidity/SwissSC.sol
@@ -0,0 +1,10 @@
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v4.0/contracts/token/ERC20/ERC20.sol";
+
+contract SwissSC is ERC20 
+{
+        
+    constructor() ERC20("SWISS Stable coin", "SwissSC") {
+        
+    } 
+
+}   
